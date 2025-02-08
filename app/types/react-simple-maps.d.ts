d640c5cedbb930fece7ba1f77639a530a1444b5f
ShidiKAY <47364468+ShidiKAY@users.noreply.github.com>
2025-02-08 15:55:45 +0700

    Update react-simple-maps.d.ts

--- app/types/react-simple-maps.d.ts
+++ app/types/react-simple-maps.d.ts
@@ -1,12 +1,38 @@
 declare module "react-simple-maps" {
   import { ComponentType, PropsWithChildren, ReactNode } from "react";
 
-  export const ComposableMap: ComponentType<PropsWithChildren<any>>;
+  interface GeoFeature {
+    type: string;
+    properties: {
+      name: string;
+      [key: string]: unknown;
+    };
+    geometry: {
+      type: string;
+      coordinates: number[][];
+    };
+  }
+
+  interface ComposableMapProps {
+    projectionConfig?: object;
+    width?: number;
+    height?: number;
+    [key: string]: unknown;
+  }
+
+  interface GeographyProps {
+    geography: GeoFeature;
+    [key: string]: unknown;
+  }
+
+  export const ComposableMap: ComponentType<
+    PropsWithChildren<ComposableMapProps>
+  >;
   export const Geographies: ComponentType<{
     geography: string | object;
-    children: (props: { geographies: any[] }) => ReactNode;
+    children: (props: { geographies: GeoFeature[] }) => ReactNode;
   }>;
-  export const Geography: ComponentType<any>;
+  export const Geography: ComponentType<GeographyProps>;
   export const ZoomableGroup: ComponentType<
     PropsWithChildren<{
       zoom?: number;
